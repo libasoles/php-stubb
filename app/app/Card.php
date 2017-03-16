23a742986c3d0136e1421474b50f38284a3d2c14
Guillermo <gperez@libasoles.com>
2017-03-16 20:00:01 -0300

    Filter by status (enabled)

--- app/app/Card.php
+++ app/app/Card.php
@@ -1,6 +1,7 @@
 <?php
 namespace App;
 
+use App\Scopes\EnabledScope;
 use Illuminate\Database\Eloquent\Model;
 
 class Card extends Model
@@ -9,6 +10,18 @@ class Card extends Model
     protected $fillable = ['name', 'content', 'enabled'];
 
     protected $hidden = ['pivot'];
+    
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new EnabledScope);
+    }
     
     public function stack()
     {
--- app/app/Stack.php
+++ app/app/Stack.php
@@ -1,6 +1,7 @@
 <?php
 namespace App;
 
+use App\Scopes\EnabledScope;
 use Illuminate\Database\Eloquent\Model;
 
 class Stack extends Model
@@ -9,6 +10,18 @@ class Stack extends Model
     protected $fillable = ['name', 'description', 'enabled'];
     protected $hidden = ['pivot'];
 
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new EnabledScope);
+    }
+    
     public function cards()
     {
         return $this->belongsToMany('App\Card');
