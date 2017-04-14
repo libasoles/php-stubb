f5ac198e78c627e53c1a7409ad361af86e4a18ed
Guillermo <gperez@libasoles.com>
2017-04-14 20:14:40 -0300

    Passport working

--- src/public/src/app_modules/common/module.js
+++ src/public/src/app_modules/common/module.js
@@ -3,10 +3,9 @@
 angular.module('app', ['ngRoute', 'app.config', 'app.home']);
   
 angular.module('app').config(['$httpProvider', function ($httpProvider) {
+ 
         
     $httpProvider.defaults.headers.common = { 
-        'Authorization': 'Bearer '+Laravel.laravel_token,
-        'access_token': Laravel.laravel_token,
         'Content-Type': 'application/json',
         'Accept': 'application/json;odata=verbose',
         'X-Login-Ajax-call': 'true',
